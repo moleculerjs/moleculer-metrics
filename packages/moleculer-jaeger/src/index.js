22e50eb6613afa40f5fe88715b4ec403c3264849
Icebob <mereg.norbert@gmail.com>
2018-03-09 21:50:01 +0100

    implement jaeger logic

--- packages/moleculer-jaeger/src/index.js
+++ packages/moleculer-jaeger/src/index.js
@@ -10,6 +10,7 @@ const Jaeger 		= require("jaeger-client");
 const UDPSender 	= require("jaeger-client/dist/src/reporters/udp_sender").default;
 const LoggingReporter 	= require("jaeger-client/dist/src/reporters/logging_reporter").default;
 const Opentracing 	= require("opentracing");
+const Int64 = require('node-int64');
 
 /**
  * Moleculer metrics module for Jaeger.
@@ -40,6 +41,7 @@ module.exports = {
 	 */
 	events: {
 		"metrics.trace.span.finish"(metric) {
+			this.logger.warn("ID: ", metric.id, " Req:", metric.requestID)
 			this.makePayload(metric);
 		}
 	},
@@ -82,11 +84,55 @@ module.exports = {
 		 * @param {Object} metric
 		 */
 		makePayload(metric) {
-			const serviceName = this.getServiceName(metric);
+			//const serviceName = this.getServiceName(metric);
 
-			const span = this.tracer.startSpan(this.getSpanName(metric));
+			let parentCtx;
+			if (metric.parent) {
+				parentCtx = new Jaeger.SpanContext(
+					this.convertID(metric.requestID), // traceId,
+					this.convertID(metric.parent), // spanId,
+					null, // parentId,
+					null, // traceIdStr
+					null, // spanIdStr
+					null, // parentIdStr
+					1, // flags
+					{}, // baggage
+					"" // debugId
+				);
+			}
 
-			span.finish();
+			const span = this.tracer.startSpan(this.getSpanName(metric), {
+				startTime: metric.startTime,
+				childOf: parentCtx,
+				tags: {
+					nodeID: metric.nodeID,
+					level: metric.level,
+					remoteCall: metric.remoteCall
+				}
+			});
+			//span.serviceName = serviceName;
+
+			const sc = span.context();
+			sc.traceId = this.convertID(metric.requestID);
+			sc.spanId = this.convertID(metric.id);
+
+			if (metric.callerNodeID)
+				this.addTags(span, "callerNodeID", metric.callerNodeID);
+
+			if (metric.params)
+				this.addTags(span, "params", metric.params);
+
+			if (metric.meta)
+				this.addTags(span, "meta", metric.meta);
+
+			if (metric.error) {
+				span.setTag(Opentracing.Tags.ERROR, true);
+				this.addTags(span, "error.message", metric.error.message);
+				this.addTags(span, "error.type", metric.error.type);
+				this.addTags(span, "error.code", metric.error.code);
+			}
+
+			span.finish(metric.endTime);
 
 			/*
 			const payload = {
@@ -142,13 +188,31 @@ module.exports = {
 		},
 
 		/**
+		 * Add tags to span
+		 *
+		 * @param {Object} span
+		 * @param {String} key
+		 * @param {any} value
+		 */
+		addTags(span, key, value) {
+			if (typeof value == "object") {
+				span.setTag(key, JSON.stringify(value));
+			} else {
+				span.setTag(key, value);
+			}
+		},
+
+		/**
 		 * Convert Context ID to Zipkin format
 		 *
 		 * @param {String} id
 		 * @returns {String}
 		 */
 		convertID(id) {
-			return id ? id.replace(/-/g, "").substring(0,16) : null;
+			if (id) {
+				return new Int64(id.replace(/-/g, "").substring(0,16)).toBuffer();
+			}
+			return null;
 		},
 
 		/**
@@ -182,9 +246,9 @@ module.exports = {
 
 		const sampler = new Jaeger.ConstSampler(1);
 		//const reporter = new Jaeger.LoggingReporter();
-		const reporter = new Jaeger.RemoteReporter(new UDPSender({host: "192.168.51.29", port: "6832", logger: this.logger }));
+		const reporter = new Jaeger.RemoteReporter(new UDPSender({host: "192.168.0.181", port: "6832", logger: this.logger }));
 		this.tracer = new Jaeger.Tracer(
-			this.name,
+			"moleculer",
 			reporter,
 			sampler,
 			{ logger: this.logger }
